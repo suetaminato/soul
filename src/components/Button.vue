a2026f90d79fd4465c8c208161fe1fa35826b387
unknown <leticiataminato@gmail.com>
2019-07-27 19:01:33 -0300

    setup e btn.js

---
+++ src/components/Button.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    class="simple-button"
+    v-bind:class="{
+      neutral: isNeutral,
+      alert: isAlert
+    }"
+  >
+    {{ msg }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    msg: String,
+    isAlert: Boolean,
+    isNeutral: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .simple-button {
+    display: block;
+    font-family: "Roboto";
+    line-height: 24px;
+    border-radius: 50px;
+    color: #FFF;
+    max-width: 250px;
+    width: 80vw;
+    border: none;
+  }
+  .neutral {
+    background-color: #48B9A8;
+    font-size: 24px;
+    padding: 24px;
+  }
+  .alert {
+    background-color: #FC6C8C;
+    font-size: 18px;
+    padding: 12px;
+  }
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>About!</h1>
+    <div class="button-list">
+      <Button msg="Entrar" isNeutral/>
+      <Button msg="Ainda não tenho conta" isAlert/>
+    </div> 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Button from '@/components/Button.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style>
+
+</style>
